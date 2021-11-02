3e302e5c73489cbc788b08257aa7d859176b009f
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-11-02 10:55:54 +0100

    guest list component example

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -2,12 +2,23 @@ import { useState } from "react";
 
 const GuestList: React.FC = () => {
   const [name, setName] = useState("");
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const onClick = () => {
+    setGuests([...guests, name]);
+    setName("");
+  };
 
   return (
     <div>
       <h3>Guest List</h3>
       <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button>Add Guest</button>
+      <button onClick={onClick}>Add Guest</button>
+      <ul>
+        {guests.map((guest) => (
+          <li key={guest}>{guest}</li>
+        ))}
+      </ul>
     </div>
   );
 };
