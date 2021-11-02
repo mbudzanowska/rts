e08397d47883db12f424ad95a485b0fee1698c19
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-11-02 11:25:59 +0100

    typescript with class components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import EventComponent from "./events/EventComponent";
+import UserSearch from "./classes/UserSearch";
 
 const App = () => {
   return (
     <div>
-      <EventComponent />
+      <UserSearch
+        users={[
+          {
+            age: 23,
+            name: "Rivas Brady",
+          },
+          {
+            age: 29,
+            name: "Gail Hess",
+          },
+          {
+            age: 31,
+            name: "Marquita Vaughn",
+          },
+          {
+            age: 32,
+            name: "Wood Horn",
+          },
+          {
+            age: 36,
+            name: "Oneill Greene",
+          },
+          {
+            age: 40,
+            name: "Sarah Jacobs",
+          },
+          {
+            age: 24,
+            name: "Adrienne Hart",
+          },
+        ]}
+      />
     </div>
   );
 };
